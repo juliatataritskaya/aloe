842082197654cc6d744a250c3318eca0a2cc191b
y_tataritskaya <j.tataritskaya@vironit.co.uk>
2018-01-17 00:35:55 +0300

    add background video; migrated all to root folder

---
+++ js/background-video.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('#intro').vide({
+    mp4: "video/ALOE_PLUS_LANZAROTE_desctop.mp4",
+    poster: "img/bg-fallback-1.png"
+  }, {
+    posterType: 'png'
+  });
+
+})(jQuery); // End of use strict
